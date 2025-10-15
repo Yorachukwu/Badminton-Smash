952384b8c4bc56286668c6e39f7fd747a3a63c1d
Yorachukwu <yorajideonwo@gmail.com>
2025-10-15 18:53:32 +0100

    Added { back to code beside Monobehaviour

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d54b860dd998b7d4dfbe89274ba8d60049b7b798d2f719e4ede778483bcabdd5
-size 3575
+oid sha256:899e3b143e5e5da6515eba47cc5c3b5fa29ab0f690db9b09680ec39ce66d7212
+size 3577
